2740f048f11bfab500b0ea330f94babdc304bd37
troylochner <troy.lochner@gmail.com>
2021-01-08 16:35:26 -0500

    Initial commit for refactoring

---
+++ src/components/Directory.js
@@ -0,0 +1,14 @@
+import React from "react";
+//import "./style.css";
+
+function Directory() {
+  return (
+
+    <div className='data-area'>
+        Data to go here.
+    </div>
+
+  );
+}
+
+export default Directory;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+//import "./Header.css";
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <p>Click on a column header to sort our results.</p>
+        </div>
+    )
+}
+
+export default Header;
